f48570f6bc99708012801ea99fd0a7a3520d4e2d
aminMuktar <60422727+aminMuktar@users.noreply.github.com>
2021-11-09 05:57:11 -0500

    Delete item directory

--- item/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import category,items
-# Register your models here.
-admin.site.register(category)
-admin.site.register(items)
--- item/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path,include
-from item import views
-urlpatterns = [
-    path('itemlist/',views.itemlist.as_view()),   
-]
